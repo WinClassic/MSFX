34b8fd8edd714698e962b8a0d25bd52d7d87287c
travis <94665268+travy-patty@users.noreply.github.com>
2022-10-05 00:11:26 +0000

    added profile

---
+++ profile/chrome/JS/additional_top_toolbars.uc.js
@@ -0,0 +1,148 @@
+﻿// 'Additional top toolbars' script for Firefox 60+ by Aris
+// - "number_of_additional_top_toolbars": set the amount of additional top toolbar
+// - "tb_label": set a toolbar name
+// - use toolbar[id^="additional_top_toolbar"] {...} to affect all toolbars at once in CSS
+
+// [!] Fix for WebExtensions with own windows by 黒仪大螃蟹 (for 1-N scripts)
+//
+// workaround on Fx 71 to save/restore toolbar visibility
+// creating an observer array always fails, so observers are created manually atm.
+
+
+Components.utils.import("resource:///modules/CustomizableUI.jsm");
+var {Services} = Components.utils.import("resource://gre/modules/Services.jsm", {});
+var appversion = parseInt(Services.appinfo.version);
+
+var AdditionalTopToolbars = {
+  init: function() {
+
+	/* blank tab workaround */
+	try {
+	  if(gBrowser.selectedBrowser.getAttribute('blank')) gBrowser.selectedBrowser.removeAttribute('blank');
+	} catch(e) {}
+	  
+	var number_of_additional_top_toolbars = 1; // max 5 to save toolbar state on Fx 71+ (add additional code at the bottom for more)
+	var tb_label = "Address";
+
+	try {
+	 if(document.getElementById('additional_top_toolbar1') == null) {
+		
+	  if(number_of_additional_top_toolbars>0 && number_of_additional_top_toolbars<6) {
+	
+	    var i=1;
+
+	    while(i<=number_of_additional_top_toolbars) {
+		
+		  if(appversion <= 62) var toptoolbar = document.createElement("toolbar");
+		  else var toptoolbar = document.createXULElement("toolbar");
+		
+		  toptoolbar.setAttribute("id", "additional_top_toolbar"+i+"");
+		  toptoolbar.setAttribute("toolbarname", tb_label+"");
+		  toptoolbar.setAttribute("toolbarname", tb_label+"");
+		  toptoolbar.setAttribute("customizable","true");
+		  toptoolbar.setAttribute("class","toolbar-primary chromeclass-toolbar browser-toolbar customization-target");
+		  toptoolbar.setAttribute("mode","icons");
+		  toptoolbar.setAttribute("iconsize","small");
+		  toptoolbar.setAttribute("toolboxid","navigator-toolbox");
+		  toptoolbar.setAttribute("context","toolbar-context-menu");
+		  toptoolbar.setAttribute("lockiconsize","true");
+		  toptoolbar.setAttribute("defaultset","spring");
+		
+		  document.querySelector('#navigator-toolbox').appendChild(toptoolbar);
+		  
+		  CustomizableUI.registerArea("additional_top_toolbar"+i+"", {legacy: true});
+		  if(appversion >= 65) CustomizableUI.registerToolbarNode(toptoolbar);
+		  
+		  // top toolbars 'collapsed' on startup
+		  //setToolbarVisibility(toptoolbar, false);
+		  
+		  try {
+			Services.prefs.getDefaultBranch("browser.additional_top_toolbar"+i+".").setBoolPref("enabled",true);
+			setToolbarVisibility(document.getElementById("additional_top_toolbar"+i+""), Services.prefs.getBranch("browser.additional_top_toolbar"+i+".").getBoolPref("enabled"));
+		  } catch(e) {}	  
+		  
+		  i++;
+		
+	    }
+	  }
+	  
+	  // CSS
+	  var sss = Components.classes["@mozilla.org/content/style-sheet-service;1"].getService(Components.interfaces.nsIStyleSheetService);
+
+	  var uri = Services.io.newURI("data:text/css;charset=utf-8," + encodeURIComponent('\
+	  \
+		toolbar[id^="additional_top_toolbar"] { \
+		  -moz-appearance: none !important; \
+		  background-color: var(--toolbar-bgcolor); \
+		  background-image: var(--toolbar-bgimage); \
+		  background-clip: padding-box; \
+		  color: var(--toolbar-color, inherit); \
+		} \
+		#main-window[customizing] toolbar[id^="additional_top_toolbar"] { \
+		  outline: 1px dashed !important; \
+		  outline-offset: -2px !important; \
+		} \
+	  \
+	  '), null, null);
+
+	  sss.loadAndRegisterSheet(uri, sss.AGENT_SHEET);
+	
+	 }
+	 
+	 
+	 if(number_of_additional_top_toolbars>=1) {
+		var observer1 = new MutationObserver(function(mutations) {
+		  mutations.forEach(function(mutation) {
+			Services.prefs.getBranch("browser.additional_top_toolbar1.").setBoolPref("enabled",!document.querySelector("#additional_top_toolbar1").collapsed);
+		  });    
+		});			 
+		observer1.observe(document.querySelector("#additional_top_toolbar1"), { attributes: true, childList: true, characterData: true });
+	  }
+	  if(number_of_additional_top_toolbars>=2) {
+		var observer2 = new MutationObserver(function(mutations) {
+		  mutations.forEach(function(mutation) {
+			Services.prefs.getBranch("browser.additional_top_toolbar2.").setBoolPref("enabled",!document.querySelector("#additional_top_toolbar2").collapsed);
+		  });    
+		});			 
+		observer2.observe(document.querySelector("#additional_top_toolbar2"), { attributes: true, childList: true, characterData: true });
+	  }
+	  if(number_of_additional_top_toolbars>=3) {
+		var observer3 = new MutationObserver(function(mutations) {
+		  mutations.forEach(function(mutation) {
+			Services.prefs.getBranch("browser.additional_top_toolbar3.").setBoolPref("enabled",!document.querySelector("#additional_top_toolbar3").collapsed);
+		  });    
+		});			 
+		observer3.observe(document.querySelector("#additional_top_toolbar3"), { attributes: true, childList: true, characterData: true });
+	  }
+	  if(number_of_additional_top_toolbars>=4) {
+		var observer4 = new MutationObserver(function(mutations) {
+		  mutations.forEach(function(mutation) {
+			Services.prefs.getBranch("browser.additional_top_toolbar4.").setBoolPref("enabled",!document.querySelector("#additional_top_toolbar4").collapsed);
+		  });    
+		});			 
+		observer4.observe(document.querySelector("#additional_top_toolbar4"), { attributes: true, childList: true, characterData: true });
+	  }
+	  if(number_of_additional_top_toolbars>=5) {
+		var observer5 = new MutationObserver(function(mutations) {
+		  mutations.forEach(function(mutation) {
+			Services.prefs.getBranch("browser.additional_top_toolbar5.").setBoolPref("enabled",!document.querySelector("#additional_top_toolbar5").collapsed);
+		  });    
+		});			 
+		observer5.observe(document.querySelector("#additional_top_toolbar5"), { attributes: true, childList: true, characterData: true });
+	  }
+
+	} catch(e){}	
+
+  }
+
+}
+
+/* initialization delay workaround */
+document.addEventListener("DOMContentLoaded", AdditionalTopToolbars.init(), false);
+
+// not needed anymore, but just in case someone prefers initialization that way
+/*
+setTimeout(function(){
+  AdditionalTopToolbars.init();
+},500);
+*/
---
+++ profile/chrome/JS/move_bars.uc.js
@@ -0,0 +1,68 @@
+// 'MoveUrlbar' script for Firefox 60+ by Aris
+// option: place urlbar on a different toolbar
+// option: place back button on a different toolbar
+// option: place back button on a different toolbar
+
+
+var {CustomizableUI} = Components.utils.import("resource:///modules/CustomizableUI.jsm", {});
+
+var navigation = CustomizableUI.AREA_NAVBAR;
+var tabs = CustomizableUI.AREA_TABSTRIP;
+var menu = CustomizableUI.AREA_MENUBAR;
+var bookmarks = CustomizableUI.AREA_BOOKMARKS;
+
+/* [target toolbar of item]
+	menu = 'menubar'
+	tabs = 'tabs toolbar'
+	bookmarks = 'bookmarks toolbar'
+	navigation='navigation toolbar'   */
+var backbutton_on_toolbar = navigation;  
+var forwardbutton_on_toolbar = navigation;
+var urlbar_on_toolbar = bookmarks;
+
+/* [target position of item]
+	0 = 1st
+	1 = 2nd
+	2 = 3rd
+	...
+	x = xth    */
+var backbutton_on_toolbar_position = 0;  
+var forwardbutton_on_toolbar_position = 1;
+var urlbar_on_toolbar_position = 0;
+
+
+var MoveUrlbar = {
+  init: function() {
+
+	try {
+	  document.getElementById('back-button').setAttribute('removable','true');
+	  document.getElementById('forward-button').setAttribute('removable','true');
+	  document.getElementById('urlbar-container').setAttribute('removable','true');
+	  document.getElementById('additional_top_toolbar1').setAttribute('fullscreentoolbar','true');
+	} catch(e){}
+
+	CustomizableUI.addWidgetToArea("back-button", backbutton_on_toolbar);
+	CustomizableUI.moveWidgetWithinArea("back-button", backbutton_on_toolbar_position);
+	CustomizableUI.addWidgetToArea("forward-button", forwardbutton_on_toolbar);
+	CustomizableUI.moveWidgetWithinArea("forward-button", forwardbutton_on_toolbar_position);
+	CustomizableUI.addWidgetToArea("urlbar-container", "additional_top_toolbar1");
+	CustomizableUI.moveWidgetWithinArea("urlbar-container", urlbar_on_toolbar_position);
+
+	document.getElementById("navigator-toolbox").appendChild(document.getElementById("TabsToolbar"));
+	
+	/*
+	try {
+	  document.getElementById('back-button').setAttribute('removable','false');
+	  document.getElementById('forward-button').setAttribute('removable','false');
+	  document.getElementById('urlbar-container').setAttribute('removable','false');
+	} catch(e){}
+	*/
+  }
+
+}
+
+//document.addEventListener("DOMContentLoaded", MoveUrlbar.init(), false);
+
+setTimeout(function(){
+  MoveUrlbar.init();
+},500);
